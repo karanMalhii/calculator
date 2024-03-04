5d78d66e976516d99a49fa8a4d568070c7de4a73
karanMalhii <karanmalhii1313@gmail.com>
2024-03-04 17:40:12 +0530

    array.from doubt clear

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let string = "";
 let buttons = document.querySelectorAll(".button");
-Array.from(buttons).forEach((button) => {
+
+buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     if (e.target.innerHTML == "=") {
       string = eval(string);
